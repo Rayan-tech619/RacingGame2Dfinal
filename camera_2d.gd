88f3af6369d31e9f2f6397502d69b5f87fa555ed
Rayan-tech619 <relmaazouzi@elpuig.xeill.net>
2026-02-02 10:58:46 +0100

    a

--- camera_2d.gd
+++ camera_2d.gd
@@ -2,7 +2,7 @@ extends Camera2D
 
 @export var zoom_normal := Vector2(0.1, 0.1)
 @export var zoom_acelerar := Vector2(0.2, 0.2) # más chico = más cerca
-@export var velocidad_zoom := 5.0
+@export var velocidad_zoom := 1.0
 
 func _process(delta):
 	if Input.is_action_pressed("ui_right"):
